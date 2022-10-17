9e1bedf58624b33b2012d800beb845e7b67752d5
Aznviben <72636205+Aznviben@users.noreply.github.com>
2022-10-17 17:14:41 -0400

    Creating Components

---
+++ src/components/experience/experience.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './experience.css'
+function Experience() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Experience
---
+++ src/components/testimonials/testimonials.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './testimonials.css'
+function Testimonials() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Testimonials
